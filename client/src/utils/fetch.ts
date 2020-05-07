2ff4ee4c085c39d2d8cf269fe0521aa50e7a608c
heathercodes <heather@looka.com>
2020-05-06 21:23:04 -0400

    create wanderlist on login

--- client/src/utils/fetch.ts
+++ client/src/utils/fetch.ts
@@ -1,15 +1,19 @@
-export const fetchAPI = async (
-    url: string,
-    method: string,
-    bodyParams?: { [key: string]: string },
-): Promise<any> => {
-    const response = await fetch(url, {
-        method,
+interface RequestBody {
+    url: string;
+    method: string;
+    body: {
+        [key: string]: string;
+    };
+}
+
+export const fetchAPI = async (request: RequestBody): Promise<any> => {
+    const response = await fetch(request.url, {
+        method: request.method,
         headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json',
         },
-        body: bodyParams ? JSON.stringify(bodyParams) : null,
+        body: request.body ? JSON.stringify(request.body) : null,
     });
 
     return response.json();
