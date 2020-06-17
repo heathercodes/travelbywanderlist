463eb8a1f06f953bdc929c070bd2bbea32a5fe55
heathercodes <heather@looka.com>
2020-06-16 20:27:07 -0400

    implement better error handling

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,6 +10,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
 
+app.use((err, req, res) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
+});
+
 const server = app.listen(PORT, () => {
     console.info(`Server started on port ${PORT}`);
 });
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -9,4 +9,12 @@ router.use('/health', healthRouter);
 router.use('/location', locationRouter);
 router.use('/collection', collectionRouter);
 
+router.all('*', (req, res, next) => {
+    const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+    err.status = 'fail';
+    err.statusCode = 404;
+
+    next(err);
+});
+
 export { router };
